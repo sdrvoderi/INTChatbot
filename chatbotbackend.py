d0785f0010a300990a9a3c814a99c1d50f200d1d
sdrvoderi <stiven.drvoderic98@gmail.com>
2021-01-01 11:33:16 +0100

    izmjena u prikazu novosti

--- chatbotbackend.py
+++ chatbotbackend.py
@@ -291,7 +291,7 @@ def Novosti():
      entries = feed.entries
      if len(entries) > 0:
          novost = random.choice(entries)
-         odgovor = f"Time: {novost.published} \n News headline: {novost.title}"
+         odgovor = f"I found this news! \nTime: {novost.published} \nNews headline: {novost.title}\nLink: {novost.link}"
      else:
          odgovor = "There is no news at this time!"   
      return odgovor      
